d2db84e4577909ab8242baee6136c3e7644f6ae3
Chris.N <cenunez47@gmail.com>
2019-02-15 10:52:54 -0800

    Added show route for each campground.

--- app.js
+++ app.js
@@ -11,22 +11,11 @@ app.set('view engine', 'ejs');
 
 const campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 const Campground = mongoose.model('Campground', campgroundSchema);
-
-// Campground.create({
-//         name: 'Mountain Goat\'s Rest',
-//         image: '/images/camp_3.jpg'
-//     }, (error, campground) => {
-//         if (error) {
-//             console.log(error);
-//         } else {
-//             console.log('New campground');
-//             console.log(campground);
-//         }
-//     });
 
 app.get('/', (req, res) => {
     res.render('landing');
@@ -37,7 +26,7 @@ app.get('/campgrounds', (req, res) => {
         if (error) {
             console.log(error)
         } else {
-            res.render('campgrounds', {campgrounds: campgrounds})
+            res.render('index', {campgrounds: campgrounds})
         }
     })
 });
@@ -45,7 +34,8 @@ app.get('/campgrounds', (req, res) => {
 app.post('/campgrounds', (req, res) => {
     let name = req.body.name;
     let image = req.body.image;
-    let newCampground = { name: name, image: image }
+    let desc = req.body.description
+    let newCampground = { name: name, image: image, description: desc }
     Campground.create(newCampground, (error, newCampground) => {
         if (error) {
             console.log(error);
@@ -57,6 +47,16 @@ app.post('/campgrounds', (req, res) => {
 
 app.get('/campgrounds/new', (req, res) => {
     res.render('new');
+});
+
+app.get('/campgrounds/:id', (req, res) => {
+    Campground.findById(req.params.id, (error, foundCampground) => {
+        if (error) {
+            console.log(error);
+        } else {
+            res.render('show', {campground: foundCampground});
+        }
+    });
 });
 
 app.listen(port, () => console.log(`Server started at port ${port}.`));
